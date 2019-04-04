b6003700be9627e9e51111988d80f0ad9dfe3b6b
AhmadG1 <ahmad.sr.yaseen@gmail.com>
2019-04-04 09:57:51 +0300

    Updates

---
+++ precache-manifest.3adf6d864813f4965715ce1149bf7d41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1486c5d03fc7f11cdb54",
+    "url": "/project-4-frontend-master/static/css/main.1e588077.chunk.css"
+  },
+  {
+    "revision": "1486c5d03fc7f11cdb54",
+    "url": "/project-4-frontend-master/static/js/main.808f9e58.chunk.js"
+  },
+  {
+    "revision": "5f47f9e9397dbd00f35c",
+    "url": "/project-4-frontend-master/static/js/runtime~main.9f66a779.js"
+  },
+  {
+    "revision": "c6bdd5b4527bd6b2aee1",
+    "url": "/project-4-frontend-master/static/css/2.6d3102d1.chunk.css"
+  },
+  {
+    "revision": "c6bdd5b4527bd6b2aee1",
+    "url": "/project-4-frontend-master/static/js/2.fd9662cf.chunk.js"
+  },
+  {
+    "revision": "693c1760d55bc3a7e2e39640012479a6",
+    "url": "/project-4-frontend-master/index.html"
+  }
+];
